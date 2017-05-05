87254d414f166ca6a8254fdda66f70907cf80c7e
John Dowd <john.dowd@inspectall.com>
2017-05-05 13:53:53 -0700

    update user logic wiht for loop to print values, business logic add return push i

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
 //Business Logic - Backend
 var pingPong = function(number) {
   var result = [];
-
-  for (var i = 0; i <= number; i++) {
-
-      if ((i % 3 === 0) && (i % 5 === 0)) {
-      result.push("Ping-Pong");
-        console.log(result);
-    } else if (i % 3 === 0) {
-      result.push("Ping");
-    } else if (i % 5 ===0) {
-      result.push("Pong");
-
-    }
+    for (var i = 1; i <= number; i++) {
+     if (i % 15 === 0) {
+       result.push("ping-pong");
+     } else if (i % 3 === 0) {
+       result.push("ping");
+     } else if (i % 5 === 0) {
+       result.push("pong");
+     } else {
+       result.push(i);
+     }
+   }
     return result;
   }
-}
-
 
 //User Interface Logic - Frontend
 $(document).ready(function(){
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
-    var playerNum = parseInt($("input#number").val())
-    $("#result").append("<p>" + pingPong(playerNum) + "</p>");
-      // $("li").prev().remove();
+    var resultUl  = $('ul#result');
+    var playerNum = parseInt($("input#number").val());
+    // Empty out unordered list
+    resultUl.empty();
+
+    // Iterate through result array and print values
+    var pingPongArray = pingPong(playerNum); // [1, 2, ping, 4]
+
+    for(var x = 0; x < pingPongArray.length; x++) {
+      resultUl.append('<li>' + pingPongArray[x] + '</li>');
+    }
+
   });
 });
