4060a47c485a5480ad41c90092f8dfc51c67e9ce
John Dowd <john.dowd@inspectall.com>
2017-05-05 10:53:21 -0700

    add else if for 3 and 5 divisable

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
 //Business Logic - Backend
+var pingPong = function(number) {
+  var result = [];
+
+  for (var i = 0; i <= number; i++) {
+
+      if ((i % 3 === 0) && (i % 5 === 0)) {
+      result.push("Ping-Pong");
+        console.log(result);
+    } else if (i % 3 === 0) {
+      result.push("Ping");
+    } else if (i % 5 ===0) {
+      result.push("Pong");
+
+    }
+    return result;
+  }
+}
+
 
 //User Interface Logic - Frontend
-$(document).ready(function)(){
+$(document).ready(function(){
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var playerNum = parseInt($("input#number").val())
-    $("#result").append("<li>") + pingPong(playerNum) + "</li>"
-      $("li").prev().remove();
+    $("#result").append("<p>" + pingPong(playerNum) + "</p>");
+      // $("li").prev().remove();
   });
 });
